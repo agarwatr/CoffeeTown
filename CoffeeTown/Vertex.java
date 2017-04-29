2b6d916f2bb9bdc8a0bb571994abb3b5144664d7
agarwatr <agarwatr@lafayette.edu>
2017-04-29 17:33:15 -0400

    Edge and Vertex
    Rough edge and vertex classes

---
+++ CoffeeTown/Vertex.java
@@ -0,0 +1,30 @@
+
+/**
+ * A class that Shop and WareHouse will extend
+ * as a controller for the vertex of the graph * 
+ * @Trisha Agarwal
+ * @04/29/2017-01
+ */
+public class Vertex
+{
+    // instance variables - replace the example below with your own
+    
+    //Location of the vertex
+    private Location location;
+    
+    //ID of the vertex
+    private int ID;
+    
+    //Edges of the vertex
+   // private Edge[] e;
+
+    /**
+     * Constructor for objects of class Vertex
+     */
+    public Vertex(int x, int y, int ID)
+    {
+       
+    }
+
+    
+}
